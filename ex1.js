8fbf9c34ef85a618484ae896709cf5be9823528d
dnlslcd <danielsalcedoq@gmail.com>
2024-08-08 18:29:01 +0200

    ex1 - otra opción

--- ex1.js
+++ ex1.js
@@ -8,20 +8,21 @@ function readAndDisplayJSON(filename) {
     const data = fs.readFileSync(filename, 'utf8');
 
     // Intentar parsear el JSON
-    try {
-        const people = JSON.parse(data);
+    const people = JSON.parse(data);
         
-        // Mostrar los nombres y correos electrónicos
-        people.forEach(person => {
-        console.log(`Name: ${person.name}, Email: ${person.email}`);
+    // Mostrar los nombres y correos electrónicos
+    people.forEach(person => {
+    console.log(`Name: ${person.name}, Email: ${person.email}`);
     });
-    } catch (error) {
-        console.log("Ha ocurrido un error al leer el fichero people1_bad.json");
-    }
+    
 }
 
+try {
 // Ejecutar la función con el nombre del archivo
 readAndDisplayJSON('people1.json');
 
 // Este fichero falla. Usar try...catch para gestionar el error
 readAndDisplayJSON('people1_bad.json');
+} catch (error) {
+    console.log("Ha ocurrido un error al leer el fichero people1_bad.json");
+}
